b521db74cead805deb722c838f682d18de0dbcda
AREMSON <habeebtoyin2021@gmail.com>
2022-01-31 10:11:25 +0100

    commit it

---
+++ ussdCode.js
@@ -0,0 +1,9 @@
+isussd= true;
+while(isussd == true){
+    let ussd = prompt("Enter USSD code");
+    if(ussd == '*123#'){
+        
+    } else {
+        alert('you enter the wrong code')
+    }
+}
